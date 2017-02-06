1fe4bccbba97be5886da326cd28748ede6527c46
Artem <1ttem12@gmail.com>
2017-02-06 11:16:07 +0100

    Dao implements DaoInterface

--- src/main/java/com/ttem/dao/Dao.java
+++ src/main/java/com/ttem/dao/Dao.java
@@ -1,13 +1,29 @@
 package com.ttem.dao;
 
-public class Dao {
+import com.ttem.data.Data;
+import com.ttem.data.SourceType;
+
+import java.util.List;
+
+public class Dao implements DaoInterface{
 
     private static Dao ourInstance = new Dao();
+
+    private SourceType sourceType;
+    private Data data;
 
     public static Dao getInstance() {
         return ourInstance;
     }
 
     private Dao() {
+    }
+
+    public User selectUserById(final int id) {
+        return this.data.selectUserById(id);
+    }
+
+    public List<User> selectAllUsers() {
+        return this.data.selectAllUsers();
     }
 }
