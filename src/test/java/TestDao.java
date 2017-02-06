5388cfed993371cd5014d9c572307ef3ba694976
Artem <1ttem12@gmail.com>
2017-02-06 13:54:17 +0100

    Test Dao update add testSetSourceOfDataWhenInputDataBase add testSetSourceOfDataWhenInputXml add testSetSourceOfDataWhenInputWebService add testSetSourceOfDataDefault

--- src/test/java/TestDao.java
+++ src/test/java/TestDao.java
@@ -1,63 +1,82 @@
 import com.ttem.dao.Dao;
-import com.ttem.dao.User;
+import com.ttem.data.*;
 import org.apache.log4j.Logger;
-import org.easymock.EasyMock;
-import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
-
-import java.util.ArrayList;
-import java.util.List;
-
 
 public class TestDao {
 
     private static Logger log = Logger.getLogger(TestDao.class);
-    private static Dao daoWithEasyMock;
 
-    @BeforeClass
-    public static void setUp() throws Exception {
-        daoWithEasyMock = EasyMock.createMock(Dao.class);
-
-        EasyMock.expect(daoWithEasyMock.selectUserById(1)).andReturn(new User(1));
-        EasyMock.expect(daoWithEasyMock.selectAllUsers()).andReturn(new ArrayList<>());
-
-        EasyMock.replay(daoWithEasyMock);
-    }
-
-    @Test
-    public void testSelectUserById() throws Exception {
-        int expectedId = 1;
-
-        int inputId = 1;
-
-        int actualId = daoWithEasyMock.selectUserById(inputId).getId();
-
-        Assert.assertEquals(expectedId, actualId);
-    }
-
-    @Test
-    public void testSelectAllUsers() throws Exception {
-        List<User> expectedArray = new ArrayList<>();
-
-        List<User> actualArray = daoWithEasyMock.selectAllUsers();
-
-        Assert.assertSame(expectedArray.getClass(), actualArray.getClass());
-    }
 
     @Test
     public void testDaoSingleton() throws Exception {
         Dao expectedDao = Dao.getInstance();
 
-        Dao testDao = Dao.getInstance();
+        Dao actualDao = Dao.getInstance();
 
-        Assert.assertSame(expectedDao,testDao);
-        log.info(expectedDao.toString() + " == " + testDao.toString());
+        Assert.assertSame(expectedDao,actualDao);
     }
 
-    @AfterClass
-    public static void verifyEasyMock() {
-        EasyMock.verify(daoWithEasyMock);
+    @Test
+    public void testSetSourceOfDataWhenInputDataBase() throws Exception {
+        SourceType expectedSourceType = SourceType.DATA_BASE;
+        Data expectedData = new DataBase();
+
+        SourceType inputSourceType = SourceType.DATA_BASE;
+
+        Dao testDao = Dao.getInstance();
+        testDao.setSourceOfData(inputSourceType);
+
+        SourceType actualSourceType = testDao.getCurrentSourceType();
+        Data actualData = testDao.getData();
+
+        Assert.assertSame(expectedSourceType, actualSourceType);
+        Assert.assertSame(expectedData.getClass(), actualData.getClass());
+    }
+
+    @Test
+    public void testSetSourceOfDataWhenInputXml() throws Exception {
+        SourceType expectedSourceType = SourceType.XML;
+        Data expectedData = new Xml();
+
+        SourceType inputSourceType = SourceType.XML;
+
+        Dao testDao = Dao.getInstance();
+        testDao.setSourceOfData(inputSourceType);
+
+        SourceType actualSourceType = testDao.getCurrentSourceType();
+        Data actualData = testDao.getData();
+
+        Assert.assertSame(expectedSourceType, actualSourceType);
+        Assert.assertSame(expectedData.getClass(), actualData.getClass());
+    }
+
+    @Test
+    public void testSetSourceOfDataWhenInputWebService() throws Exception {
+        SourceType expectedSourceType = SourceType.WEB_SERVICE;
+        Data expectedData = new WebService();
+
+        SourceType inputSourceType = SourceType.WEB_SERVICE;
+
+        Dao testDao = Dao.getInstance();
+        testDao.setSourceOfData(inputSourceType);
+
+        SourceType actualSourceType = testDao.getCurrentSourceType();
+        Data actualData = testDao.getData();
+
+        Assert.assertSame(expectedSourceType, actualSourceType);
+        Assert.assertSame(expectedData.getClass(), actualData.getClass());
+    }
+
+    @Test
+    public void testSetSourceOfDataDefault() throws Exception {
+        SourceType expectedSourceType = Dao.getDefaultSourceType();
+
+        Dao testDao = Dao.getInstance();
+
+        SourceType actualSourceType = testDao.getCurrentSourceType();
+
+        Assert.assertSame(expectedSourceType, actualSourceType);
     }
 }
