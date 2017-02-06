18e32cc10100cc4a20ea5cc7848716b6774da157
Artem <1ttem12@gmail.com>
2017-02-06 13:55:19 +0100

    Create TestDaoEasyMock add testSelectUserById   with easyMock add testSelectAllUsers  with easyMock

---
+++ src/test/java/TestDaoEasyMock.java
@@ -0,0 +1,51 @@
+import com.ttem.dao.Dao;
+import com.ttem.dao.User;
+import org.easymock.EasyMock;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestDaoEasyMock {
+
+    private static Dao daoWithEasyMock;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        TestDaoEasyMock.daoWithEasyMock = EasyMock.createMock(Dao.class);
+
+        EasyMock.expect(TestDaoEasyMock.daoWithEasyMock.selectUserById(1)).andReturn(new User(1));
+        EasyMock.expect(TestDaoEasyMock.daoWithEasyMock.selectAllUsers()).andReturn(new ArrayList<>());
+
+        EasyMock.replay(TestDaoEasyMock.daoWithEasyMock);
+    }
+
+    @Test
+    public void testSelectUserByIdWithEasyMock() throws Exception {
+        int expectedId = 1;
+
+        int inputId = 1;
+
+        int actualId = TestDaoEasyMock.daoWithEasyMock.selectUserById(inputId).getId();
+
+        Assert.assertEquals(expectedId, actualId);
+    }
+
+    @Test
+    public void testSelectAllUsersWithEasyMock() throws Exception {
+        List<User> expectedArray = new ArrayList<>();
+
+        List<User> actualArray = TestDaoEasyMock.daoWithEasyMock.selectAllUsers();
+
+        Assert.assertSame(expectedArray.getClass(), actualArray.getClass());
+    }
+
+
+    @AfterClass
+    public static void verifyEasyMock() {
+        EasyMock.verify(TestDaoEasyMock.daoWithEasyMock);
+    }
+}
