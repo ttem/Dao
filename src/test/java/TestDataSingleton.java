37578293709ab595005bdd96dbaaba476bc55d8e
Artem <1ttem12@gmail.com>
2017-02-08 20:37:44 +0100

    added TestDataSingleton

---
+++ src/test/java/TestDataSingleton.java
@@ -0,0 +1,35 @@
+import com.ttem.data.DataBase;
+import com.ttem.data.WebService;
+import com.ttem.data.Xml;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestDataSingleton {
+
+    @Test
+    public void dataBase() {
+        DataBase expectedDataBase = DataBase.getInstance();
+
+        DataBase actualDataBase = DataBase.getInstance();
+
+        Assert.assertSame(expectedDataBase, actualDataBase);
+    }
+
+    @Test
+    public void xml() throws Exception {
+        Xml expectedXml = Xml.getInstance();
+
+        Xml actualXml = Xml.getInstance();
+
+        Assert.assertSame(expectedXml, actualXml);
+    }
+
+    @Test
+    public void webService() throws Exception {
+        WebService expectedWebService = WebService.getInstance();
+
+        WebService actualWebService = WebService.getInstance();
+
+        Assert.assertSame(expectedWebService, actualWebService);
+    }
+}
